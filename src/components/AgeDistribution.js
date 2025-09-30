f1fa9288d0161f8b3787bee193d1d84b0aeab02f
Hunter Evanoff <hunter547@gmail.com>
2025-09-30 13:30:38 -0600

    Remove unused imports.

--- src/components/AgeDistribution.js
+++ src/components/AgeDistribution.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Box, Typography, Paper } from '@mui/material';
-import { Male, Female } from '@mui/icons-material';
 
 const AgeDistribution = ({ ageData, title, color, icon }) => {
   if (!ageData) return null;
